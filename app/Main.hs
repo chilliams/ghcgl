d4e5957f3bb30da582e54c8914f44a5952774354
Chris Williams <chilliams@gmail.com>
2017-11-02 09:00:03 -0400

    Color cubes

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,16 @@
 module Main where
 
 import Graphics.UI.GLUT
- 
+import Bindings
+
+myPoints :: [(GLfloat,GLfloat,GLfloat)]
+myPoints = [ (sin (2*pi*k/12), cos (2*pi*k/12), 0) | k <- [1..12] ]
+
 main :: IO ()
 main = do
   (_progName, _args) <- getArgsAndInitialize
-  _window <- createWindow "Hello World"
+  _window <- createWindow "TypesCraft"
   displayCallback $= display
+  reshapeCallback $= Just reshape
+  keyboardMouseCallback $= Just keyboardMouse
   mainLoop
- 
-display :: DisplayCallback
-display = do
-  clear [ ColorBuffer ]
-  flush
