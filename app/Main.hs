287ebdbe081bcde52f025dc2a34112ae5b3e87db
Chris Williams <chilliams@gmail.com>
2017-11-02 09:21:35 -0400

    Cubes

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,20 @@
 module Main where
 
 import Graphics.UI.GLUT
+import Data.IORef
 import Bindings
-
-myPoints :: [(GLfloat,GLfloat,GLfloat)]
-myPoints = [ (sin (2*pi*k/12), cos (2*pi*k/12), 0) | k <- [1..12] ]
 
 main :: IO ()
 main = do
   (_progName, _args) <- getArgsAndInitialize
-  _window <- createWindow "TypesCraft"
-  displayCallback $= display
+  initialDisplayMode $= [WithDepthBuffer, DoubleBuffered]
+  _window <- createWindow "Hello World"
   reshapeCallback $= Just reshape
-  keyboardMouseCallback $= Just keyboardMouse
+  depthFunc $= Just Less -- the comparison function for depth the buffer
+  angle <- newIORef 0
+  delta <- newIORef 0.1
+  pos <- newIORef (0, 0)
+  keyboardMouseCallback $= Just (keyboardMouse delta pos)
+  idleCallback $= Just (idle angle delta)
+  displayCallback $= display angle pos
   mainLoop
